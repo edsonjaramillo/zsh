ceb98deace6fc7d9faa207511ecd019a1a601690
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-21 23:51:33 -0500

    refactor: Update download_and_extract_repo function in refresh-omp-them.zsh

--- scripts/refresh-omp-them.zsh
+++ scripts/refresh-omp-them.zsh
@@ -1,3 +1,3 @@
 source $HOME/zshrc.shared.d/functions/files.zsh
 
-download_and_extract_repo https://github.com/edsonjaramillo/oh-my-posh/archive/refs/heads/main.tar.gz
+download_and_extract_repo https://github.com/edsonjaramillo/oh-my-posh.git main $HOME/themes
