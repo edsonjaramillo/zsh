2764a267f97e8ddb281e06e126523a5a618489d5
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-21 23:05:41 -0500

    refactor: Update source path for oh-my-posh in refresh-omp-them.zsh

--- scripts/refresh-omp-them.zsh
+++ scripts/refresh-omp-them.zsh
@@ -1,3 +1,3 @@
-source $HOME/zshrc.d/functions/shared/files.zsh
+source $HOME/zshrc.shared.d/functions/files.zsh
 
 download_and_extract_repo https://github.com/edsonjaramillo/oh-my-posh/archive/refs/heads/main.tar.gz
