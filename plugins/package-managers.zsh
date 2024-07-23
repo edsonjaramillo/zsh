ef6c43c6ac9ce3b992254d9e1907ee51b3131f57
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-23 14:21:34 -0500

    refactor: Update npm path in package-managers.zsh

--- plugins/package-managers.zsh
+++ plugins/package-managers.zsh
@@ -1,5 +1,5 @@
 # add npm to path if homebrew is installed
 if [[ -d /opt/homebrew ]]; then
-    node_version=$(ls /opt/homebrew/opt/node@* | sort -V | tail -n 1)
-    export PATH="/opt/homebrew/opt/node@${node_version}/bin:$PATH"
+    node_version=$(ls /opt/homebrew/opt/ | grep node@)
+    export PATH="/opt/homebrew/opt/$node_version/bin:$PATH"
 fi
