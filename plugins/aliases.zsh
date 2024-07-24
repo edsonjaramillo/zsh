3e9216279efd5c4961c6a1376202df9de2ff97b6
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-24 00:17:05 -0500

    feat(aliases): add aliases for lsd commands

--- plugins/aliases.zsh
+++ plugins/aliases.zsh
@@ -1,3 +1,9 @@
+# Aliases for common commands
 alias cl="clear"
 alias rs="source $HOME/.zshrc"
 alias paths='echo $PATH | tr ":" "\n"'
+
+# lsd command aliases
+alias ltree='lsd --tree'
+alias l='lsd'
+alias la='lsd -a'ß
